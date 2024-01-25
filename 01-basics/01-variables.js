2eafe25d8f522dd516513e53dea4c91f598eda17
Vishal Yadav <vy372984@gmail.com>
2024-01-25 16:25:33 +0530

    Initial commit

---
+++ 01-basics/01-variables.js
@@ -0,0 +1,18 @@
+const vishalId = 1455;
+let vishalMail = "vishal@098gmail.com";
+var vishalPass = "233344";
+vishalCity = "JAIPUR";
+let accountState;
+
+// vishalId = 2;
+// console.log(vishalId);
+vishalMail = " vc.hc@gm.com";
+vishalPass = "21221312";
+vishalCity = "Jaipur";
+
+console.table([vishalId, vishalMail, vishalPass, vishalCity, accountState]);
+/*
+
+use let and const
+
+*/
