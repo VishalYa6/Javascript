d95c30fda450b87bbd55aeeeda9fedbddbe2f416
Vishal Yadav <vy372984@gmail.com>
2024-01-25 23:36:59 +0530

    objects

---
+++ 02-basics/objects.js
@@ -0,0 +1,37 @@
+//singleton
+
+// object.create
+//objects literals
+
+const mySym = Symbol("key1");
+
+const JsUser = {
+  name: "Hitesh",
+  "full name": "Vishal Yadav",
+  [mySym]: "mykey1" ,
+  age: 54,
+  location: "jaipur",
+  email: "hitesh@gmail.com",
+  isLoggedIn: false,
+  lastLoginDays: ["Monday", "Saturday"],
+};
+
+// console.log(JsUser.email);
+// console.log(JsUser["email"]);
+// console.log(JsUser["full name"]);
+console.log(JsUser[mySym]);
+// JsUser.email = "vishal@gmail.com";
+// console.log(JsUser.email);
+// Object.freeze(JsUser);
+// JsUser.email = "vishal@avl.com";
+
+
+JsUser.greeting = function(){
+    console.log("hello js user");
+}
+JsUser.greetingTwo = function(){
+    console.log(`hello js user, ${this.name}`);
+}
+
+console.log(JsUser.greetingTwo());
+console.log(JsUser.greeting());
