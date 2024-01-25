2b6b1a68a483555747a8fab060c00bc5cd2132d9
Vishal Yadav <vy372984@gmail.com>
2024-01-25 16:33:48 +0530

    data_types

--- 01-basics/data-types.js
+++ 01-basics/data-types.js
@@ -1,1 +1,7 @@
-"use strict";
+"use strict"; //treat all js code as newer version
+
+// alert(3+3) we are using node js , not browser
+
+console.log(3 + 3);
+// code readbility should high
+console.log("Vishal");
