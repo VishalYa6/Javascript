eb10b6ed0713a27a75b63dc040f73c19df0868a7
Vishal Yadav <vy372984@gmail.com>
2024-01-25 18:41:18 +0530

    strings

---
+++ 01-basics/Strings.js
@@ -0,0 +1,37 @@
+// const name = " Vishal"
+// const repoCount = 100
+
+// // console.log(name + repoCount + " Value");
+// // console.log(`hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('Vishal-hs-comm')
+
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('s'));
+ 
+
+// const newString = gameName.substring(0,4) //ignore negative values
+// console.log(newString);
+
+// const anotherString = gameName.slice(-8,4)
+// console.log(anotherString);
+
+// const newStringOne = "      hitesh      "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+
+
+// const url = "https://google.com/google%20google"
+
+// // console.log(url.replace('%20','-'));
+// console.log(url.includes('google'));
+
+
+// console.log(gameName.split('-')); // split on basis of dash
+
