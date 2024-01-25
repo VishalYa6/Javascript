9e543afba215214e7c172666a5ca771de94128eb
Vishal Yadav <vy372984@gmail.com>
2024-01-25 23:09:28 +0530

    arrays

---
+++ 02-basics/02-arrays.js
@@ -0,0 +1,32 @@
+const marvel_heroes = ["thor", "Ironman", "spiderman"]
+const dc = ["superman" , "flash" , "batman"]
+
+// marvel_heroes.push(dc)
+
+// console.log(marvel_heroes);
+// console.log(marvel_heroes[3][1]);
+
+
+// const allHeroes = marvel_heroes.concat(dc);
+// console.log(allHeroes);
+
+// const allHeroes = [...marvel_heroes, ...dc]
+// console.log(allHeroes);
+
+// const anotherArray = [1,2, 3, [4, 5, 6] , 7 , [6 , 7 , [4, 5]] ]
+const usableArray = anotherArray.flat(Infinity)
+// console.log(usableArray);
+
+// console.log(Array.isArray("Vishal"));
+// console.log(Array.from("Vishal"));
+// console.log(Array.from({name: "Vishal"}));//empty array
+
+let score1 = 100 
+let score2 = 200 
+let score3 = 300
+
+console.log(Array.of(score1, score2 , score3));
+
+
+
+
