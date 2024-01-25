7b54df0e835942f870ec7554d07937ae8d1060db
Vishal Yadav <vy372984@gmail.com>
2024-01-25 16:47:48 +0530

    data-types

--- 01-basics/data-types.js
+++ 01-basics/data-types.js
@@ -5,3 +5,21 @@
 console.log(3 + 3);
 // code readbility should high
 console.log("Vishal");
+
+let name = "vishal"; //string
+
+let age = 19; // number
+let isLoggedIn = false;
+let state = null;
+
+// number => 2 to power 54
+// bigInt
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => uniqueness
+
+// object
+console.log(typeof undefined);//undefined
+console.log(typeof null ) // object -> output
