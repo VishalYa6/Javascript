d6d582a114dea907d647a409bd264c565a879487
Vishal Yadav <vy372984@gmail.com>
2024-01-25 17:39:01 +0530

    comparison

---
+++ 01-basics/comparison.js
@@ -0,0 +1,15 @@
+// console.log("2">1);
+// console.log("02" > 1);
+
+// console.log(null > 0);//avoid
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined == 0);//avoid
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+// === checks deeply
+
+console.log("2" == 2);
+console.log("2" === 2);
