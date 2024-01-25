c6f8550d6448cbc03b33fb0fa2d43408681b5e2f
Vishal Yadav <vy372984@gmail.com>
2024-01-25 17:08:01 +0530

    conversions

---
+++ 01-basics/conversion.js
@@ -0,0 +1,32 @@
+// let age = "vishal";
+
+// console.log(typeof age);
+// console.log(typeof age);
+
+// let valueInNumber = Number(age);
+// console.log(typeof valueInNumber);
+// console.log( valueInNumber);
+// NaN -> not a number
+
+// notes :
+// 33 => 33
+// "33abc" = NaN
+// TRUE = 1
+// FALSE = 0
+
+// let isLoggedIn = "";
+
+// let booleanIsloggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsloggedIn);
+
+// 1 => true ; 0 => false
+// " " => false
+// "vishal " => true
+
+// let someVsh = 33;
+// let stringNumber = String(someVsh);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);//string 
+
+
+
