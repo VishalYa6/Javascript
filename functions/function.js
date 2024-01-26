96fcea3c4342fd1615239e6379dfbd4d58dacfcc
Vishal Yadav <vy372984@gmail.com>
2024-01-26 14:51:21 +0530

    function

---
+++ functions/function.js
@@ -0,0 +1,46 @@
+// console.log("H");
+// console.log("I");
+// console.log("T");
+// console.log("s");
+
+
+// function sayMyName(){
+//     console.log("H");
+//  console.log("I");
+//  console.log("T");
+//  console.log("s");
+
+// }
+
+// sayMyName()
+
+
+// function addTwoNo(number1 , number2){
+    
+//     console.log( number1+number2);
+// }
+
+// function addTwoNo(number1 , number2){
+    
+// //    let result = number1 + number2
+// //    return result
+//    return number1 + number2
+// }
+
+// const result = addTwoNo(1, 5)
+
+// console.log(result); // undefined 
+
+// function loginUserMessage(username = "sam"){
+//     if(!username){
+//            console.log("Please enter username");
+//            return 
+//     }
+//     return `${username} just logged in`
+// }
+
+
+// // console.log(loginUserMessage("vishal"));
+
+// console.log(loginUserMessage("hitesh"));
+
