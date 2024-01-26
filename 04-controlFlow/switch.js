062edd6cca3c6e5195d145cbf162aea491c675c0
Vishal Yadav <vy372984@gmail.com>
2024-01-26 17:43:30 +0530

    control-flow

---
+++ 04-controlFlow/switch.js
@@ -0,0 +1,30 @@
+//switch case 
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "april"
+
+switch (month) {
+    case "januaray":
+        console.log("january");
+        break;
+    case "feb":
+        console.log("feburary");
+        break;
+    case "march":
+        console.log("march");
+        break;
+
+    case "april":
+        console.log("april");
+        break;
+    default:
+       console.log("no month");
+        break;
+}
