5c0157d289d858a857f4a2ca0c5e4d25122f9d8a
Vishal Yadav <vy372984@gmail.com>
2024-01-28 14:59:40 +0530

    await

--- Advance-javascript/async-await/async-await.js
+++ Advance-javascript/async-await/async-await.js
@@ -1,10 +1,16 @@
+// async function getData() {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       resolve(455);
+//     }, 3500);
+//   });
+// }
 async function getData() {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve(455);
-    }, 3500);
-  });
-}
+   let x = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+   let data = await x.json()   
+    console.log(data);
+    
+  }
 async function main() {
   console.log("Loading modules");
   console.log("superman");
